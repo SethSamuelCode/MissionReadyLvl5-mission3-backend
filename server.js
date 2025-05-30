9fc1f61a64c6567f1823ccba18fd30e111d7b360
Seth Samuel <Seths@missionreadyhq.com>
2025-05-30 16:13:31 +1200

    wip

--- server.js
+++ server.js
@@ -9,12 +9,14 @@ const cors = require("cors"); // CORS middleware
 const PORT = process.env.SERVER_LISTEN_PORT; // Port from environment
 const assert = require("node:assert/strict"); // Assertion utility for debugging
 
-const { GoogleGenAI } = require("@google/genai");
+const { GoogleGenAI, Chat, Models } = require("@google/genai");
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+
 // --------------------- MIDDLEWARES -------------------- //
 
 const morgan = require("morgan"); // HTTP request logger
+const send = require("send");
 app.use(morgan("dev")); // Log requests to console
 app.use(express.json({ limit: "10MB" })); // Parse JSON bodies up to 10MB.
 
@@ -35,7 +37,48 @@ app.use(cors(corsConfigs)); // Apply CORS policy
 
 // ---------------------- FUNCTIONS --------------------- //
 
+// ----------------------- DEFINES ---------------------- //
+
+const MODEL_NAME = "gemini-2.0-flash-001";
+
+class ChatSession {
+  constructor(session) {
+    const aiConfig = {
+      responseMimeType: "text/plain",
+      systemInstruction: [
+        {
+          text: `you are a bird and you have to work "caw caw" into every line `,
+        },
+      ],
+    };
+
+    this.lastContact = new Date();
+    this._session = ai.chats.create(aiConfig, MODEL_NAME);
+  }
+
+  get session() {
+    this.lastContact = new Date();
+    return this._session;
+  }
+
+
+}
+
+const chatSessions = new Map();
 // ----------------------- ROUTES ----------------------- //
+
+app.post("/api/chat", async (req, resp) => {
+  const userInput = req.body.userInput;
+  const job = req.body.job;
+  const uuid = req.body.uuid;
+
+  if (!chatSessions.has(uuid)) {
+    chatSessions.set(uuid, new ChatSession());
+  }
+
+  const fromAi = await chatSessions.get(uuid).session.sendMessage({ message: userInput });
+  resp.send(fromAi.text)
+});
 
 app.post("/api/aiTest", async (req, resp) => {
   const userInput = req.body.userInput;
