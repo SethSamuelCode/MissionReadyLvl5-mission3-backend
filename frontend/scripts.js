bc5b40cbc7be955927b9571f005da6757d88258f
Seth Samuel <Seths@missionreadyhq.com>
2025-05-31 22:20:29 +1200

    chats now display on the screen from both user and AI

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,34 +1,42 @@
-"use strict"
+"use strict";
 console.log("hello world");
 
-const textArea = document.querySelector("#convoDisplayArea")
-const userInputForm = document.querySelector("#userInputForm")
-const userTextInputArea = document.querySelector("#userInput")
-const jobDescriptionInput = document.querySelector("#jobDescription")
-userInputForm.addEventListener("submit",sendToAI)
+const conversationArea = document.querySelector("#convoDisplayArea");
+const userInputForm = document.querySelector("#userInputForm");
+const userTextInputArea = document.querySelector("#userInput");
+const jobDescriptionInput = document.querySelector("#jobDescription");
+userInputForm.addEventListener("submit", sendToAI);
 
-async function sendToAI(e){
-    e.preventDefault()
-    const userInput= userTextInputArea.value;
-    console.log(userInput)
-    userTextInputArea.value=""
+async function sendToAI(e) {
+  e.preventDefault();
+  const userInput = userTextInputArea.value;
+  console.log(userInput);
+  userTextInputArea.value = "";
 
-    const fetchOptions ={
-        method: 'post',
-            headers: {
-        'Content-Type': 'application/json'
+  const fetchOptions = {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
     },
-      body: JSON.stringify({
-        uuid:44332,
-        job:jobDescriptionInput.value,
-        userInput:userInput
-      })  
-    }
+    body: JSON.stringify({
+      uuid: 44332,
+      job: jobDescriptionInput.value,
+      userInput: userInput,
+    }),
+  };
+  const userInputElement = document.createElement("p");
+  userInputElement.classList.add("userInputBubble");
+  userInputElement.classList.add("bubble");
+  userInputElement.insertAdjacentText("afterbegin", userInput);
+  conversationArea.append(userInputElement);
 
+  const aiResponse = await fetch("http://localhost:4000/api/chat", fetchOptions);
+  const aiText = await aiResponse.json();
+  console.log(aiText.response);
 
-    const aiResponse= await fetch("http://localhost:4000/api/chat",fetchOptions)
-    const aiText = await aiResponse.json()
-    console.log(aiText.response)
-
-
+  const aiResponseElement = document.createElement("p");
+  aiResponseElement.classList.add("aiResponseBubble");
+  aiResponseElement.classList.add("bubble");
+  aiResponseElement.insertAdjacentText("afterbegin", aiText.response);
+  conversationArea.append(aiResponseElement);
 }
