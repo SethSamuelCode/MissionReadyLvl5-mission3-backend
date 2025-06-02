accdf57f469b8f2544e3eb51dc8d6bbc40ae2041
Valentine Ncube <valentinemelly77@gmail.com>
2025-06-03 06:05:25 +1200

    created LogoHeader component and exported it to InterviewApp.jsx

---
+++ frontEnd2/mock-interview/src/InterviewApp.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Logo from './components/LogoHeader.jsx'
+
+function InterviewApp() {
+  ;<div className="app-container">
+    <Logo />
+  </div>
+}
---
+++ frontEnd2/mock-interview/src/components/LogoHeader.jsx
@@ -0,0 +1,10 @@
+function Logo() {
+  return (
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuUSrMhuoa9oRL7pyUTPJASr16X0Pm6Om8yQ&s"
+      alt="Turners Logo"
+      classname="logo"
+    />
+  )
+}
+export default Logo
