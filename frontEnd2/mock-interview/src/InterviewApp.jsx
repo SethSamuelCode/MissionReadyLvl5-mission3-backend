176d814f31ccd23dbe69884d8e86375f603eff64
Valentine Ncube <valentinemelly77@gmail.com>
2025-06-03 10:15:35 +1200

    created, imported and styled  UserInput.jsx

--- frontEnd2/mock-interview/src/InterviewApp.jsx
+++ frontEnd2/mock-interview/src/InterviewApp.jsx
@@ -1,20 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Logo from './components/LogoHeader.jsx'
 import JobTitleInput from './components/JobTitleInput.jsx'
 import StartButton from './components/StartButton.jsx'
+import UserInput from './components/UserInput.jsx'
 
 function InterviewApp() {
   const [jobTitle, setJobTitle] = useState('')
+  const [userResponse, setUserResponse] = useState('')
 
   const handleStart = async () => {
     if (!jobTitle.trim()) return alert('Please enter a job title')
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    //API call to send user input
   }
 
   return (
     <div className="app-container">
       <Logo />
       <JobTitleInput jobTitle={jobTitle} setJobTitle={setJobTitle} />
+
       {<StartButton onClick={handleStart} />}
+      {
+        <UserInput
+          userResponse={userResponse}
+          setUserResponse={setUserResponse}
+          handleSubmit={handleSubmit}
+        />
+      }
     </div>
   )
 }
