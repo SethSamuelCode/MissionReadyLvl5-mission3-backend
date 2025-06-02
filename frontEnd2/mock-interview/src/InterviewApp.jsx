74e7dcc282e69b600733629f78309f9611a4d562
Valentine Ncube <valentinemelly77@gmail.com>
2025-06-03 07:17:39 +1200

    created, exported  and styled JobTitleInput.jsx component

--- frontEnd2/mock-interview/src/InterviewApp.jsx
+++ frontEnd2/mock-interview/src/InterviewApp.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Logo from './components/LogoHeader.jsx'
+import JobTitleInput from './components/JobTitleInput.jsx'
 
 function InterviewApp() {
   return (
     <div className="app-container">
       <Logo />
+      <JobTitleInput />
     </div>
   )
 }
